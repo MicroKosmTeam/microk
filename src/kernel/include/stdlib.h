b2c516709956e9902eb289ae997a61a1747216e6
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-29 19:21:11 +0100

    Added RENAME(f) macro

--- src/kernel/include/stdlib.h
+++ src/kernel/include/stdlib.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <stdbool.h>
+#include <stdio.h>
 #include <cdefs.h>
-#include <sys/printk.h>
-#include <io/io.h>
-#include <mm/kmalloc.h>
+
+#ifndef PREFIX
+        #define RENAME(f)
+#else
+       #define RENAME(f) PREFIX ## f
+#endif
+// Example: rename printk to printf
+// #define printf RENAME(printk)
 
 #ifdef __cplusplus
 extern "C" {
