7ea939f0a95267e6f6425cfb612e4a8f849b090f
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-30 17:59:35 +0100

    Added some comments in panik.c

--- src/kernel/sys/panik.c
+++ src/kernel/sys/panik.c
@@ -1,9 +1,9 @@
 #include <sys/panik.h>
 
 void panik(const char *message, const char *file, const char *function, unsigned int line) {
-        asm volatile ("cli");
+        asm volatile ("cli"); // We don't want interrupts while we are panicking
 #ifdef KCONSOLE_SERIAL
-        // Do shit
+        // Nothing to do for now
 #endif
 #ifdef KCONSOLE_VGA
         vga_print_set_color(PRINT_COLOR_RED, PRINT_COLOR_WHITE);
@@ -14,12 +14,16 @@ void panik(const char *message, const char *file, const char *function, unsigned
 #ifdef KCONSOLE_GOP
         gop_print_set_color(PRINT_COLOR_RED, PRINT_COLOR_WHITE);
 #endif
-        printk("\n\n!!PANIK!!\n\n");
+        // Printing the panic message
+        printk("\n\n!! PANIK!! \n");
+        printk("Irrecoverable error in the kernel.\n\n");
         printk("%s version %s, build %s %s\n", KNAME, KVER, __DATE__, __TIME__);
         printk("%s in function %s at line %d\n", file, function, line);
         printk("Cause: %s\n", message);
         printk("[Hanging now...]\n");
+
         while (true) {
+                // Halting forever
                 asm volatile ("hlt");
         }
 
