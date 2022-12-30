62a45585ab29d84e0ef48740b5fa7bc2dba4eac2
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-30 17:58:58 +0100

    Added inw outw

--- src/kernel/io/io.c
+++ src/kernel/io/io.c
@@ -4,9 +4,19 @@ inline void outb(uint16_t port, uint8_t val) {
         asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
 
+inline void outw(uint16_t port, uint16_t val) {
+        asm volatile ( "outw %0, %1" : : "a"(val), "Nd"(port) );
+}
+
 inline uint8_t inb(uint16_t port) {
         uint8_t ret;
         asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+        return ret;
+}
+
+inline uint16_t inw(uint16_t port) {
+        uint16_t ret;
+        asm volatile ( "inw %1, %0" : "=a"(ret) : "Nd"(port) );
         return ret;
 }
 
