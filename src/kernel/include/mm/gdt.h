f60086cf79a0586229994b44e977f1398b580149
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-30 18:00:32 +0100

    Added additional gdt flags.

--- src/kernel/include/mm/gdt.h
+++ src/kernel/include/mm/gdt.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define GDT_CODE      (3<<11)
-#define GDT_DPL(lvl)  ((lvl)<<13)
-#define GDT_PRESENT   (1<<15)
-#define GDT_LONG      (1<<21)
+#define GDT_CODE        (0x18<<8)
+#define GDT_DATA        (0x12<<8)
+#define GDT_TSS         (0x09<<8)
+#define GDT_DPL(lvl)    ((lvl)<<13)
+#define GDT_PRESENT     (1<<15)
+#define GDT_LONG        (1<<21)
