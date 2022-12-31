cfbe9893a82375ed6bfa888c775d32884e8c2663
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-31 19:26:15 +0100

    New boot config variables.

--- src/kernel/include/cdefs.h
+++ src/kernel/include/cdefs.h
@@ -5,8 +5,12 @@
 #define KVER "devel-alpha"
 #define KCOMMENTS "Merry Christmas!"
 
-#define KBOOT_MBOOT1
-#define KBOOT_MBOOT2
+#define KBOOT_EFI
+#undef  KBOOT_GRUB
+#ifdef  KBOOT_GRUB
+        #define KBOOT_MBOOT1
+        #define KBOOT_MBOOT2
+#endif
 #define KMAX_CPUS 16
 #define KMAX_IOAPIC 8
 #define KCONSOLE_SERIAL
