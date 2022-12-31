808d8faac0eda3f4e93beb3b66a3b6f0a53b919e
Filippo Mutta <filippo.mutta@gmail.com>
2022-12-31 09:07:20 +0100

    Added new cflags.

--- src/kernel/include/cdefs.h
+++ src/kernel/include/cdefs.h
@@ -4,6 +4,9 @@
 #define KNAME "MicroKernel"
 #define KVER "devel-alpha"
 #define KCOMMENTS "Merry Christmas!"
+
+#define KBOOT_MBOOT1
+#define KBOOT_MBOOT2
 #define KMAX_CPUS 16
 #define KMAX_IOAPIC 8
 #define KCONSOLE_SERIAL
