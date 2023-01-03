c4368bdeb0518d517c08dcf20b48191150b88483
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-03 18:58:36 +0100

    Added memset function.

--- src/kernel/mm/memory.cpp
+++ src/kernel/mm/memory.cpp
@@ -14,3 +14,9 @@ uint64_t get_memory_size(EFI_MEMORY_DESCRIPTOR *mMap, uint64_t mMapEntries, uint
 
         return memory_size_bytes;
 }
+
+void memset(void *start, uint8_t value, uint64_t num) {
+        for (uint64_t i = 0; i < num; i++) {
+                *(uint8_t*)((uint64_t)start + i) = value;
+        }
+}
