3fcb4c96c13b6ba149484efde051b029aa2cab18
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-04 20:20:47 +0100

    Added functions to the GOP

--- src/kernel/dev/fb/gop.cpp
+++ src/kernel/dev/fb/gop.cpp
@@ -14,18 +14,92 @@ GOP::GOP(Framebuffer *new_target_framebuffer, PSF1_FONT *new_psf1_font) {
         target_framebuffer = new_target_framebuffer;
         psf1_font = new_psf1_font;
         
+        height = target_framebuffer->Height;
+        width = target_framebuffer->Width;
         front_color = 0xffffffff;
         back_color = 0x00000000;
         cursor_position = {0, 0};
 }
 
 void GOP::print_clear() {
-        memset(target_framebuffer->BaseAddress, back_color, target_framebuffer->BufferSize);
         cursor_position.Y = 0;
         cursor_position.X = 0;
+
+        uint64_t fb_base = (uint64_t)target_framebuffer->BaseAddress;
+        uint64_t bps = target_framebuffer->PixelsPerScanLine * 4;
+        uint64_t fb_height = target_framebuffer->Height;
+
+        for (int vertical_scanline = 0; vertical_scanline < fb_height; vertical_scanline++) {
+                uint64_t pixPtrBase = fb_base + (bps * vertical_scanline);
+                for (uint32_t *pixPtr =(uint32_t*)pixPtrBase; pixPtr < (uint32_t*)(pixPtrBase+bps); pixPtr++) {
+                        *pixPtr = back_color;
+                }
+        }
+}
+        
+uint32_t GOP::get_pixel(uint32_t x, uint32_t y) {
+        return *(uint32_t*)((uint64_t)target_framebuffer->BaseAddress + (x*4) + (y*target_framebuffer->PixelsPerScanLine*4));
+}
+
+void GOP::put_pixel(uint32_t x, uint32_t y, uint32_t color) {
+        *(uint32_t*)((uint64_t)target_framebuffer->BaseAddress + (x*4) + (y*target_framebuffer->PixelsPerScanLine*4)) = color;
+}
+
+void GOP::clear_mouse_cursor(uint8_t *mouse_pointer, Point pos) {
+        if (!mouse_drawn) return;
+        int xmax = 16;
+        int ymax = 16;
+        int diffx = width - pos.X;
+        int diffy = height - pos.Y;
+
+        if(diffx < 16) xmax = diffx;
+        if(diffy < 16) ymax = diffy;
+
+        for (int y = 0; y < ymax; y++) {
+                for (int x = 0; x < xmax; x++) {
+                        int bit = y *16 + x;
+                        int byte = bit / 8;
+                        if((mouse_pointer[byte] & (0b10000000 >> (x % 8)))) {
+                                if (get_pixel(pos.X +x, pos.Y +y) == MouseCursorBufferAfter[x+y*16]) {
+                                        put_pixel(pos.X + x, pos.Y + y, MouseCursorBuffer[x+y*16]);
+                                }
+
+                        }
+                }
+        }
+}
+
+void GOP::draw_mouse_cursor(uint8_t *mouse_pointer, Point pos, uint32_t color) {
+        int xmax = 16;
+        int ymax = 16;
+        int diffx = width - pos.X;
+        int diffy = height - pos.Y;
+
+        if(diffx < 16) xmax = diffx;
+        if(diffy < 16) ymax = diffy;
+
+        for (int y = 0; y < ymax; y++) {
+                for (int x = 0; x < xmax; x++) {
+                        int bit = y *16 + x;
+                        int byte = bit / 8;
+                        if((mouse_pointer[byte] & (0b10000000 >> (x % 8)))) {
+                                MouseCursorBuffer[x+y*16] = get_pixel(pos.X + x, pos.Y + y);
+                                put_pixel(pos.X + x, pos.Y + y, color);
+                                MouseCursorBufferAfter[x+y*16] = get_pixel(pos.X + x, pos.Y + y);
+                        }
+                }
+        }
+
+        mouse_drawn = true;
+
 }
 
 void GOP::print_char(const char character) {
+        if(cursor_position.X + 8 > target_framebuffer->Width) {
+                cursor_position.X = 0;
+                cursor_position.Y += 16;
+        }
+
         switch (character) {
         case '\n':
                 cursor_position.X=0;
@@ -48,19 +122,44 @@ void GOP::print_char(const char character) {
 
                         fontPtr++;
                 }
+
                 cursor_position.X += 8;
+        }
+}
+
+void GOP::print_char_pos(const char character, Point pos) {
+        Point oldpos = cursor_position;
+        cursor_position = pos;
+        print_char(character);
+        cursor_position = oldpos;
+}
+
+void GOP::clear_char() {
+        if (cursor_position.X == 0) {
+                if (cursor_position.Y >=16) {
+                        cursor_position.X = target_framebuffer->Width;
+                        cursor_position.Y -= 16;
+                } else {
+                        return;
+                }
+        } 
+
+        cursor_position.X -= 8;
+
+        unsigned int* pixPtr = (unsigned int*)target_framebuffer->BaseAddress;
+        for (unsigned long y = cursor_position.Y; y < cursor_position.Y + 16; y++){
+                for (unsigned long x = cursor_position.X; x < cursor_position.X+8; x++){
+                        *(unsigned int*)(pixPtr + x + (y * target_framebuffer->PixelsPerScanLine)) = back_color;
+                }
         }
 }
 
 void GOP::print_str(const char* string) {
         char *character = (char*)string;
         while(*character != 0) {
-                print_char(*character);
 
-                if(cursor_position.X + 8 > target_framebuffer->Width) {
-                        cursor_position.X = 0;
-                        cursor_position.Y += 16;
-                }
+                
+                print_char(*character); 
 
                 character++;
         }
--- src/kernel/include/dev/fb/gop.h
+++ src/kernel/include/dev/fb/gop.h
@@ -5,19 +5,30 @@
 #include <dev/fb/fb.h>
 #include <dev/fb/fonts.h>
 
-
 class GOP{
 public:
         GOP();
         GOP(Framebuffer *new_target_framebuffer, PSF1_FONT *new_psf1_font);
         void print_clear();
+        void clear_char();
         void print_char(const char character);
+        void print_char_pos(const char character, Point pos);
         void print_str(const char* string);
         void print_set_color(const unsigned int foreground, const unsigned int background);
 
+        uint32_t get_pixel(uint32_t x, uint32_t y);
+        void put_pixel(uint32_t x, uint32_t y, uint32_t color);
+
+        uint32_t MouseCursorBuffer[16*16];
+        uint32_t MouseCursorBufferAfter[16*16];
+        void clear_mouse_cursor(uint8_t *mouse_pointer, Point pos);
+        void draw_mouse_cursor(uint8_t *mouse_pointer, Point pos, uint32_t color);
+
+        unsigned int height, width;
+        bool mouse_drawn;
 private:
-        Point cursor_position;
         Framebuffer *target_framebuffer;
+        Point cursor_position;
         PSF1_FONT *psf1_font;
         unsigned int back_color;
         unsigned int front_color;
