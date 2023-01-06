b3ac12b3e00d98dabbf8c9a00f4f6b0819fb40a7
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-06 15:07:14 +0100

    Added basic tss.

--- src/kernel/cpu/gdt.cpp
+++ src/kernel/cpu/gdt.cpp
@@ -1,10 +1,28 @@
 #include <cpu/gdt.h>
+#include <mm/memory.h>
 
 __attribute((aligned(0x1000))) GDT DefaultGDT = {
         {0, 0, 0, 0x00, 0x00, 0}, // Null
         {0, 0, 0, 0x9a, 0xa0, 0}, // Kcode
         {0, 0, 0, 0x92, 0xa0, 0}, // Kdata
-        {0, 0, 0, 0x00, 0x00, 0}, // Unull
+        {0, 0, 0, 0x00, 0x00, 0}, // UNull
         {0, 0, 0, 0x9a, 0xa0, 0}, // Ucode
         {0, 0, 0, 0x92, 0xa0, 0}, // Udata
+        {0, 0, 0, 0x00, 0x00, 0}, // TSS
 };
+
+tss_entry tss;
+void init_tss() {
+        DefaultGDT.TSS.limit0 = sizeof(tss);
+        DefaultGDT.TSS.base0 = 0;
+        DefaultGDT.TSS.base1 = 0;
+        DefaultGDT.TSS.AccessByte = 0x89;
+        DefaultGDT.TSS.limit1_Flags = 0x00;
+        DefaultGDT.TSS.base2 = 0;
+        DefaultGDT.TSS.base3 = (uint64_t)&tss;
+
+        memset(&tss, 0, sizeof(tss));
+        
+//        tss.ss0
+//        tss.esp0
+}
--- src/kernel/include/cpu/gdt.h
+++ src/kernel/include/cpu/gdt.h
@@ -6,12 +6,23 @@ struct GDTDescriptor {
         uint64_t offset;
 }__attribute__((packed));
 
-struct GDTEntry {uint16_t limit0;
+struct GDTEntry {
+        uint16_t limit0;
         uint16_t base0;
         uint8_t base1;
         uint8_t AccessByte;
         uint8_t limit1_Flags;
         uint8_t base2;
+}__attribute__((packed));
+
+struct GDTEntryTSS {
+        uint16_t limit0;
+        uint16_t base0;
+        uint8_t base1;
+        uint8_t AccessByte;
+        uint8_t limit1_Flags;
+        uint8_t base2;
+         uint64_t base3;
 }__attribute__((packed));
 
 struct GDT {
@@ -21,8 +32,42 @@ struct GDT {
         GDTEntry UserNull;
         GDTEntry UserCode;
         GDTEntry UserData;
+        GDTEntryTSS TSS;
 }__attribute__((packed)) __attribute__((aligned(0x1000)));
+
+struct tss_entry {
+	uint32_t prev_tss; // The previous TSS - with hardware task switching these form a kind of backward linked list.
+	uint32_t esp0;     // The stack pointer to load when changing to kernel mode.
+	uint32_t ss0;      // The stack segment to load when changing to kernel mode.
+	// Everything below here is unused.
+	uint32_t esp1; // esp and ss 1 and 2 would be used when switching to rings 1 or 2.
+	uint32_t ss1;
+	uint32_t esp2;
+	uint32_t ss2;
+	uint32_t cr3;
+	uint32_t eip;
+	uint32_t eflags;
+	uint32_t eax;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t ebx;
+	uint32_t esp;
+	uint32_t ebp;
+	uint32_t esi;
+	uint32_t edi;
+	uint32_t es;
+	uint32_t cs;
+	uint32_t ss;
+	uint32_t ds;
+	uint32_t fs;
+	uint32_t gs;
+	uint32_t ldt;
+	uint16_t trap;
+	uint16_t iomap_base;
+}__attribute__((packed));
 
 extern GDT DefaultGDT;
 
 extern "C" void LoadGDT(GDTDescriptor *gdtDescriptor);
+
+void init_tss();
