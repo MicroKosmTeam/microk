550b95fee4952f9af3f5bf9338bf239051b55bbc
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-06 11:53:31 +0100

    New string functions.

--- src/kernel/include/mm/string.h
+++ src/kernel/include/mm/string.h
@@ -2,5 +2,9 @@
 #include <cdefs.h>
 #include <stddef.h>
 
+size_t strlen(const char *str);
+int strcmp(const char *s1, const char *s2);
+char *strtok(char *s, char d);
+
 void itoa (char *buf, int base, long long int d);
 long long int atoi(char *str);
--- src/kernel/mm/string.cpp
+++ src/kernel/mm/string.cpp
@@ -1,4 +1,75 @@
-#include <string.h>
+#include <mm/string.h>
+#include <mm/heap.h>
+
+size_t strlen(const char *str) {
+        const char *s;
+        for (s = str; *s; ++s);
+        return (s - str);
+}
+
+int strcmp(const char *s1, const char *s2) {
+        const unsigned char *p1 = (const unsigned char *)s1;
+        const unsigned char *p2 = (const unsigned char *)s2;
+
+        while (*p1 != '\0') {
+                if (*p2 == '\0') return  1;
+                if (*p2 > *p1)   return -1;
+                if (*p1 > *p2)   return  1;
+
+                p1++;
+                p2++;
+        }
+
+        if (*p2 != '\0') return -1;
+
+        return 0;
+}
+
+// BEWARE: this can't be used before the heap is initialized
+// That should never be a problem
+char *strtok(char *s, char d) {
+        // Stores the state of string
+        static char *input = NULL;
+ 
+        // Initialize the input string
+        if (s != NULL)
+        input = s;
+
+        // Case for final token
+        if (input == NULL)
+        return NULL;
+ 
+        // Stores the extracted string
+        char* result = new char[strlen(input) + 1];
+        int i = 0;
+ 
+        // Start extracting string and
+        // store it in array
+        for (; input[i] != '\0'; i++) {
+
+                // If delimiter is not reached
+                // then add the current character
+                // to result[i]
+                if (input[i] != d)
+                        result[i] = input[i];
+ 
+                // Else store the string formed
+                else {
+                        result[i] = '\0';
+                        input = input + i + 1;
+                        return result;
+                }
+        }
+ 
+        // Case when loop ends
+        result[i] = '\0';
+        input = NULL;
+
+        // Return the resultant pointer
+        // to the string
+        return result;
+}
+
 
 void itoa (char *buf, int base, long long int d) {
         char *p = buf;
