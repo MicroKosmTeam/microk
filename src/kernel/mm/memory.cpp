d299c7c5f9410f18affd8677c3baf7a84ee169c7
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-12 19:42:57 +0100

    memcmp added.

--- src/kernel/mm/memory.cpp
+++ src/kernel/mm/memory.cpp
@@ -15,6 +15,18 @@ uint64_t get_memory_size(EFI_MEMORY_DESCRIPTOR *mMap, uint64_t mMapEntries, uint
         return memory_size_bytes;
 }
 
+extern "C" int memcmp(const void* buf1, const void* buf2, size_t count) {
+    if(!count)
+        return(0);
+
+    while(--count && *(char*)buf1 == *(char*)buf2 ) {
+        buf1 = (char*)buf1 + 1;
+        buf2 = (char*)buf2 + 1;
+    }
+
+    return(*((unsigned char*)buf1) - *((unsigned char*)buf2));
+}
+
 extern "C" void memset(void *start, uint8_t value, uint64_t num) {
         for (uint64_t i = 0; i < num; i++) {
                 *(uint8_t*)((uint64_t)start + i) = value;
