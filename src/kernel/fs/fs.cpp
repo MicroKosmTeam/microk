fa533de341034553f29f2c55850e7ee3bd407aa1
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-23 19:23:13 +0100

    Slow but stable. Will fix soon. At least we aren't limited by our DMA buffer size.

--- src/kernel/fs/fs.cpp
+++ src/kernel/fs/fs.cpp
@@ -67,13 +67,21 @@ FSManager::FSManager() {
 bool FSManager::ReadDrive(uint8_t drive_number, uint32_t start_sector, uint32_t number_sectors, uint8_t **buffer, size_t buffer_size, size_t sector_size) {
         switch(supportedDrives[drive_number].driveType) {
                 case DriveType::AHCI: {
+                        for (uint64_t base = 0; base < buffer_size; base += sector_size) {
+                                supportedDrives[drive_number].driver.ahciDriver.port->Read(start_sector + base / sector_size, 1, supportedDrives[drive_number].driver.ahciDriver.port->buffer);
+                                memcpy(*buffer + base,
+                                       supportedDrives[drive_number].driver.ahciDriver.port->buffer,
+                                       sector_size > buffer_size ? buffer_size : sector_size);
+                        }
+/*
                         for (uint64_t base = 0; base < buffer_size; base += supportedDrives[drive_number].driver.ahciDriver.buffer_size) {
                                 supportedDrives[drive_number].driver.ahciDriver.port->Read(start_sector + base * sector_size, number_sectors, supportedDrives[drive_number].driver.ahciDriver.port->buffer);
                                 memcpy(*buffer + base,
                                        supportedDrives[drive_number].driver.ahciDriver.port->buffer,
                                        supportedDrives[drive_number].driver.ahciDriver.buffer_size > buffer_size ? buffer_size : supportedDrives[drive_number].driver.ahciDriver.buffer_size);
                         }
-                        /*supportedDrives[drive_number].driver.ahciDriver.port->Read(start_sector, number_sectors, supportedDrives[drive_number].driver.ahciDriver.port->buffer);
+                        
+                        supportedDrives[drive_number].driver.ahciDriver.port->Read(start_sector, number_sectors, supportedDrives[drive_number].driver.ahciDriver.port->buffer);
                         memcpy(*buffer,
                                supportedDrives[drive_number].driver.ahciDriver.port->buffer,
                                buffer_size);*/
