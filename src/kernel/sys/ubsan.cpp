2d883b1b4731f986b8ffb23796a8451d734b27a2
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-26 18:41:54 +0100

    Changed function name from printk_location to print_location in sys/ubsan.cpp.

--- src/kernel/sys/ubsan.cpp
+++ src/kernel/sys/ubsan.cpp
@@ -3,8 +3,6 @@
 #include <sys/panik.h>
 #include <fs/vfs.h>
 
-
-
 const char *Type_Check_Kinds[] = {
     "load of",
     "store to",
@@ -17,39 +15,31 @@ const char *Type_Check_Kinds[] = {
     "upcast of",
     "cast to virtual base of",
 };
-#ifdef __cplusplus
-extern "C" {
-#endif
-static void printk_location(struct source_location *location) {
+
+static void print_location(struct source_location *location) {
     fprintf(VFS_FILE_STDLOG, " file: %s\n line: %d\n column: %d\n",
          location->file, location->line, location->column);
 }
 
 void __ubsan_handle_missing_return() {
-
 }
 
 void __ubsan_handle_divrem_overflow() {
-
 }
 
 void __ubsan_handle_load_invalid_value() {
-
 }
 
 void __ubsan_handle_shift_out_of_bounds() {
-
 }
 
 void __ubsan_handle_builtin_unreachable() {
-
 }
 
 void __ubsan_handle_mul_overflow() {
 }
 
 void __ubsan_handle_negate_overflow() {
-
 }
 
 void __ubsan_handle_out_of_bounds(void* data_raw, void* index_raw) {
@@ -61,7 +51,7 @@ void __ubsan_handle_out_of_bounds(void* data_raw, void* index_raw) {
                 fprintf(VFS_FILE_STDLOG, "\n!!! UBSAN !!!\n"
                        "Out of bounds!\n");
 
-                printk_location(&data->location);
+                print_location(&data->location);
         #endif
  
         #ifdef UBSAN_PANIK
@@ -70,28 +60,27 @@ void __ubsan_handle_out_of_bounds(void* data_raw, void* index_raw) {
 }
 
 void __ubsan_handle_pointer_overflow() {
-
 }
 void __ubsan_handle_add_overflow() {
-
 }
 void __ubsan_handle_sub_overflow() {
-
 }
+
 void __ubsan_handle_type_mismatch_v1(struct type_mismatch_info *type_mismatch, uintptr_t pointer) {
         struct source_location *location = &type_mismatch->location;
 
         if (pointer == 0) {
                 #ifdef UBSAN_NULL_PTR
+                        fprintf(VFS_FILE_STDLOG, "\n!!! UBSAN !!!\n");
                         fprintf(VFS_FILE_STDLOG, "Null pointer access!\n");
-                        printk_location(location);
+                        print_location(location);
                 #endif
         } else if (type_mismatch->alignment != 0 && is_aligned(pointer, type_mismatch->alignment)) {
                 // Most useful on architectures with stricter memory alignment requirements, like ARM.
                 #ifdef UBSAN_MEM_ALIGN
                         fprintf(VFS_FILE_STDLOG, "\n!!! UBSAN !!!\n");
                         fprintf(VFS_FILE_STDLOG, "Unaligned memory access!\n"); 
-                        printk_location(location);
+                        print_location(location);
                 #endif
         } else {
                 #ifdef UBSAN_INSUFFSIZE
@@ -100,7 +89,7 @@ void __ubsan_handle_type_mismatch_v1(struct type_mismatch_info *type_mismatch, u
                         fprintf(VFS_FILE_STDLOG, "%s address 0x%x with insufficient space for object of type %s\n",
                                 Type_Check_Kinds[type_mismatch->type_check_kind], (void *)pointer,
                                 type_mismatch->type->name);
-                        printk_location(location);
+                        print_location(location);
                 #endif
         }
 
@@ -108,6 +97,4 @@ void __ubsan_handle_type_mismatch_v1(struct type_mismatch_info *type_mismatch, u
                 PANIK("UBSAN Panicked.");
         #endif
 }
-#ifdef __cplusplus
-}
-#endif
+
