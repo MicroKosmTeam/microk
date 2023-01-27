71762fd13175454e199a8f5db07d2c3039064c36
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-27 19:12:44 +0100

    Added GetVendor function.

--- src/kernel/cpu/cpu.cpp
+++ src/kernel/cpu/cpu.cpp
@@ -4,7 +4,6 @@
 #include <cpuid.h>
 
 #define PREFIX "[CPU] "
-
 #define CPUID_VENDOR_AMD           "AuthenticAMD"
 #define CPUID_VENDOR_INTEL         "GenuineIntel"
 #define CPUID_VENDOR_QEMU          "TCGTCGTCGTCG"
@@ -12,78 +11,38 @@
 #define CPUID_VENDOR_VMWARE        "VMwareVMware"
 #define CPUID_VENDOR_VIRTUALBOX    "VBoxVBoxVBox"
 #define CPUID_VENDOR_HYPERV        "Microsoft Hv"
-enum {
-    CPUID_FEAT_ECX_SSE3         = 1 << 0, 
-    CPUID_FEAT_ECX_PCLMUL       = 1 << 1,
-    CPUID_FEAT_ECX_DTES64       = 1 << 2,
-    CPUID_FEAT_ECX_MONITOR      = 1 << 3,  
-    CPUID_FEAT_ECX_DS_CPL       = 1 << 4,  
-    CPUID_FEAT_ECX_VMX          = 1 << 5,  
-    CPUID_FEAT_ECX_SMX          = 1 << 6,  
-    CPUID_FEAT_ECX_EST          = 1 << 7,  
-    CPUID_FEAT_ECX_TM2          = 1 << 8,  
-    CPUID_FEAT_ECX_SSSE3        = 1 << 9,  
-    CPUID_FEAT_ECX_CID          = 1 << 10,
-    CPUID_FEAT_ECX_SDBG         = 1 << 11,
-    CPUID_FEAT_ECX_FMA          = 1 << 12,
-    CPUID_FEAT_ECX_CX16         = 1 << 13, 
-    CPUID_FEAT_ECX_XTPR         = 1 << 14, 
-    CPUID_FEAT_ECX_PDCM         = 1 << 15, 
-    CPUID_FEAT_ECX_PCID         = 1 << 17, 
-    CPUID_FEAT_ECX_DCA          = 1 << 18, 
-    CPUID_FEAT_ECX_SSE4_1       = 1 << 19, 
-    CPUID_FEAT_ECX_SSE4_2       = 1 << 20, 
-    CPUID_FEAT_ECX_X2APIC       = 1 << 21, 
-    CPUID_FEAT_ECX_MOVBE        = 1 << 22, 
-    CPUID_FEAT_ECX_POPCNT       = 1 << 23, 
-    CPUID_FEAT_ECX_TSC          = 1 << 24, 
-    CPUID_FEAT_ECX_AES          = 1 << 25, 
-    CPUID_FEAT_ECX_XSAVE        = 1 << 26, 
-    CPUID_FEAT_ECX_OSXSAVE      = 1 << 27, 
-    CPUID_FEAT_ECX_AVX          = 1 << 28,
-    CPUID_FEAT_ECX_F16C         = 1 << 29,
-    CPUID_FEAT_ECX_RDRAND       = 1 << 30,
-    CPUID_FEAT_ECX_HYPERVISOR   = 1 << 31,
- 
-    CPUID_FEAT_EDX_FPU          = 1 << 0,  
-    CPUID_FEAT_EDX_VME          = 1 << 1,  
-    CPUID_FEAT_EDX_DE           = 1 << 2,  
-    CPUID_FEAT_EDX_PSE          = 1 << 3,  
-    CPUID_FEAT_EDX_TSC          = 1 << 4,  
-    CPUID_FEAT_EDX_MSR          = 1 << 5,  
-    CPUID_FEAT_EDX_PAE          = 1 << 6,  
-    CPUID_FEAT_EDX_MCE          = 1 << 7,  
-    CPUID_FEAT_EDX_CX8          = 1 << 8,  
-    CPUID_FEAT_EDX_APIC         = 1 << 9,  
-    CPUID_FEAT_EDX_SEP          = 1 << 11, 
-    CPUID_FEAT_EDX_MTRR         = 1 << 12, 
-    CPUID_FEAT_EDX_PGE          = 1 << 13, 
-    CPUID_FEAT_EDX_MCA          = 1 << 14, 
-    CPUID_FEAT_EDX_CMOV         = 1 << 15, 
-    CPUID_FEAT_EDX_PAT          = 1 << 16, 
-    CPUID_FEAT_EDX_PSE36        = 1 << 17, 
-    CPUID_FEAT_EDX_PSN          = 1 << 18, 
-    CPUID_FEAT_EDX_CLFLUSH      = 1 << 19, 
-    CPUID_FEAT_EDX_DS           = 1 << 21, 
-    CPUID_FEAT_EDX_ACPI         = 1 << 22, 
-    CPUID_FEAT_EDX_MMX          = 1 << 23, 
-    CPUID_FEAT_EDX_FXSR         = 1 << 24, 
-    CPUID_FEAT_EDX_SSE          = 1 << 25, 
-    CPUID_FEAT_EDX_SSE2         = 1 << 26, 
-    CPUID_FEAT_EDX_SS           = 1 << 27, 
-    CPUID_FEAT_EDX_HTT          = 1 << 28, 
-    CPUID_FEAT_EDX_TM           = 1 << 29, 
-    CPUID_FEAT_EDX_IA64         = 1 << 30,
-    CPUID_FEAT_EDX_PBE          = 1 << 31
+
+enum CPU_VENDOR {
+        CPU_VENDOR_AMD,
+        CPU_VENDOR_INTEL,
+        CPU_VENDOR_QEMU,
+        CPU_VENDOR_KVM,
+        CPU_VENDOR_VMWARE,
+        CPU_VENDOR_VIRTUALBOX,
+        CPU_VENDOR_HYPERV,
 };
+
+static uint8_t cpuVendors = 7;
+static const char *cpuVendorNames[] = {
+        CPUID_VENDOR_AMD,
+        CPUID_VENDOR_INTEL,
+        CPUID_VENDOR_QEMU,
+        CPUID_VENDOR_KVM,
+        CPUID_VENDOR_VMWARE,
+        CPUID_VENDOR_VIRTUALBOX,
+        CPUID_VENDOR_HYPERV,
+};
+
 namespace CPU {
+        CPU_VENDOR vendor;
+
         static int __checkSSE(void) {
                 unsigned int eax, unused, edx;
                 __cpuid(1, eax, unused, unused, edx);
                 return edx & CPUID_FEAT_EDX_SSE;
         }
 
-        static void __getVendor() {
+        static CPU_VENDOR __getVendor() {
                 uint32_t ebx, edx, ecx, unused;
                 __cpuid(0, unused, ebx, edx, ecx);
                 char model[13];
@@ -102,10 +61,19 @@ namespace CPU {
                 model[12] = '\0';
 
                 fprintf(VFS_FILE_STDLOG, PREFIX "CPU vendor is: %s\n", model);
+                if(strcmp(model, cpuVendorNames[0]) == 0)
+                        return CPU_VENDOR_AMD;
+                if(strcmp(model, cpuVendorNames[1]) == 0)
+                        return CPU_VENDOR_INTEL;
+                
+        }
+
+        const char *GetVendor() {
+                return cpuVendorNames[vendor];
         }
 
         void Init() {
-                __getVendor();
+                vendor = __getVendor();
 
                 if(__checkSSE()) {
                         #ifdef x86_64
@@ -116,5 +84,6 @@ namespace CPU {
                                 fprintf(VFS_FILE_STDLOG, PREFIX "SSE status: Not present\n");
                         #endif
                 }
+                
         }
 }
