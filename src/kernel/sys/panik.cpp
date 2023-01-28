b4ba8aac8387bcd7fa18460ff33f7d0c5c6d7b17
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-28 19:37:36 +0100

    Stack unwinding in panik.

--- src/kernel/sys/panik.cpp
+++ src/kernel/sys/panik.cpp
@@ -2,6 +2,25 @@
 #include <sys/printk.h>
 #include <cdefs.h>
 
+/* Assume, as is often the case, that EBP is the first thing pushed. If not, we are in trouble. */
+struct stackframe {
+  struct stackframe* rbp;
+  uint64_t rip;
+};
+
+static void UnwindStack(int MaxFrames) {
+    struct stackframe *stk;
+    stk = (stackframe*)__builtin_frame_address(0);
+    //asm volatile ("mov %%rbp,%0" : "=r"(stk) ::);
+    printk("Stack trace:\n");
+    for(unsigned int frame = 0; stk && frame < MaxFrames; ++frame)
+    {
+        // Unwind to previous stack frame
+        printk("  0x%x     \n", stk->rip);
+        stk = stk->rbp;
+    }
+}
+
 void panik(const char *message, const char *file, const char *function, unsigned int line) {
         asm volatile ("cli"); // We don't want interrupts while we are panicking
 
@@ -15,6 +34,7 @@ void panik(const char *message, const char *file, const char *function, unsigned
         printk("%s version %s, build %s %s\n", KNAME, KVER, __DATE__, __TIME__);
         printk("%s in function %s at line %d\n", file, function, line);
         printk("Cause: %s\n", message);
+	UnwindStack(5);
         printk("[Hanging now...]\n");
 
         while (true) {
