8fbae026f4eb8e41a53f1d398fd27de0b1496caa
Filippo Mutta <filippo.mutta@gmail.com>
2023-01-28 19:38:29 +0100

    Trying stdout and stdlog files.

--- src/kernel/fs/vfs.cpp
+++ src/kernel/fs/vfs.cpp
@@ -1,8 +1,39 @@
 #include <fs/vfs.h>
 #include <sys/printk.h>
+#include <stdio.h>
+#include <mm/heap.h>
+
+FILE *stdout = NULL;
+FILE *stdlog = NULL;
+
+void VFS_Init() {
+	stdout = (FILE*)malloc(sizeof(stdout));
+	stdlog = (FILE*)malloc(sizeof(stdlog));
+	stdout->descriptor = VFS_FILE_STDOUT;
+	stdlog->descriptor = VFS_FILE_STDLOG;
+}
 
 int VFS_WriteFile(fd_t file, uint8_t *data, size_t size) {
         switch (file) {
+                case VFS_FILE_STDOUT: {
+                        char ch = data[0];
+                        GOP_print_char(ch);
+                        }
+                        return 0;
+                case VFS_FILE_STDIN:
+                        return 0;
+                case VFS_FILE_STDERR:
+                        return 0;
+                case VFS_FILE_STDLOG: {
+                        char ch = data[0];
+                        serial_print_char(ch);
+                        }
+                        return 0;
+        }
+}
+
+int VFS_Write(FILE *file, uint8_t *data, size_t size) {
+        switch (file->descriptor) {
                 case VFS_FILE_STDOUT: {
                         char ch = data[0];
                         GOP_print_char(ch);
--- src/kernel/include/fs/vfs.h
+++ src/kernel/include/fs/vfs.h
@@ -7,14 +7,25 @@
 #define VFS_FILE_STDERR 2
 #define VFS_FILE_STDLOG 3
 
+typedef struct {
+	uint32_t descriptor;
+} FILE;
+
 typedef int fd_t;
+
+extern FILE *stdout;
+extern FILE *stdlog;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+void VFS_Init();
+
 int VFS_WriteFile(fd_t file, uint8_t *data, size_t size);
+int VFS_Write(FILE *file, uint8_t *data, size_t size);
 
 #ifdef __cplusplus
 }
 #endif
+
