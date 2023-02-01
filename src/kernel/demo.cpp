98fb63ea09d11b3662f42f52a6ec2bd31eda2803
Filippo Mutta <filippo.mutta@gmail.com>
2023-02-01 19:31:51 +0100

    Raytracing demo.

--- src/kernel/demo.cpp
+++ src/kernel/demo.cpp
@@ -5,8 +5,109 @@
 #include <mm/memory.h>
 #include <mm/heap.h>
 #include <dev/timer/pit/pit.h>
+#include <sys/cstr.h>
+#include <math.h>
+
+namespace RAYTRACER {
+
+struct Vec3 {
+  double x,y,z;
+  Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+  Vec3 operator + (const Vec3& v) const { return Vec3(x+v.x, y+v.y, z+v.z); }
+  Vec3 operator - (const Vec3& v) const { return Vec3(x-v.x, y-v.y, z-v.z); }
+  Vec3 operator * (double d) const { return Vec3(x*d, y*d, z*d); }
+  Vec3 operator / (double d) const { return Vec3(x/d, y/d, z/d); }
+  Vec3 normalize() const {
+    double mg = sqrt(x*x + y*y + z*z);
+    return Vec3(x/mg,y/mg,z/mg);
+  }
+};
+inline double dot(const Vec3& a, const Vec3& b) {
+  return (a.x*b.x + a.y*b.y + a.z*b.z);
+}
+
+struct Ray {
+  Vec3 o,d;
+  Ray(const Vec3& o, const Vec3& d) : o(o), d(d) {}
+};
+
+struct Sphere {
+  Vec3 c;
+  double r;
+  Sphere(const Vec3& c, double r) : c(c), r(r) {}
+  Vec3 getNormal(const Vec3& pi) const { return (pi - c) / r; }
+  bool intersect(const Ray& ray, double &t) const {
+    const Vec3 o = ray.o;
+    const Vec3 d = ray.d;
+    const Vec3 oc = o - c;
+    const double b = 2 * dot(oc, d);
+    const double c = dot(oc, oc) - r*r;
+    double disc = b*b - 4 * c;
+    if (disc < 1e-4) return false;
+    disc = sqrt(disc);
+    const double t0 = -b - disc;
+    const double t1 = -b + disc;
+    t = (t0 < t1) ? t0 : t1;
+    return true;
+  }
+};
+
+void clamp255(Vec3& col) {
+	col.x = (col.x > 255) ? 255 : (col.x < 0) ? 0 : col.x;
+	col.y = (col.y > 255) ? 255 : (col.y < 0) ? 0 : col.y;
+	col.z = (col.z > 255) ? 255 : (col.z < 0) ? 0 : col.z;
+}
+
+void Raytrace() {
+	const int H = 640;
+	const int W = 640;
+
+	const Vec3 white(255, 255, 255);
+	const Vec3 black(0, 0, 0);
+	const Vec3 red(255, 0, 0);
+
+	const Sphere sphere(Vec3(W*0.5, H*0.5, 50), 50);
+
+	for (int i = 0; i < 15; i++) {
+		const Sphere light(Vec3(i * 50, 0, 50), 1);
+
+		double t;
+		Vec3 pix_col(black);
+
+		for (int y = 0; y < H; ++y) {
+			for (int x = 0; x < W; ++x) {
+				pix_col = black;
+				const Ray ray(Vec3(x,y,0),Vec3(0,0,1));
+
+				if (sphere.intersect(ray, t)) {
+					const Vec3 pi = ray.o + ray.d*t;
+					const Vec3 L = light.c - pi;
+					const Vec3 N = sphere.getNormal(pi);
+					const double dt = dot(L.normalize(), N.normalize());
+					pix_col = (red + white*dt) * 0.5;
+					clamp255(pix_col);
+				}
+
+				uint32_t color = ((uint8_t)pix_col.z | ((uint8_t)pix_col.y << 8) | ((uint8_t)pix_col.x << 16) | (0xff << 24));
+				GOP_print_pixel(x, y, color);
+			}
+		}
+	}
+
+	for (int i = 0; i < 30; i++) printf("\n");
+
+	return;
+}
+
+}
 
 namespace DEMO {
+
+void Raytrace() {
+	RAYTRACER::Raytrace();
+	return;
+}
+
 void Stress() {
         uint32_t malloc_count = 100;
         uint32_t memcpy_count = 100;
@@ -83,6 +184,7 @@ void Stress() {
                 free(src);
                 free(dest);
         }
+
 }
 
 void Doom() {
--- src/kernel/include/demo.h
+++ src/kernel/include/demo.h
@@ -1,7 +1,9 @@
 #pragma once
 
 namespace DEMO {
+	void Raytrace();
         void Stress();
         void Doom();
         void BadApple();
 }
+
