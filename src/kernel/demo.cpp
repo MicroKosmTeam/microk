df4080ae7b8923e4385684ad24e99ebb19337ab7
Filippo Mutta <filippo.mutta@gmail.com>
2023-02-02 18:43:23 +0100

    Enhanced raytracer demo.

--- src/kernel/demo.cpp
+++ src/kernel/demo.cpp
@@ -11,45 +11,45 @@
 namespace RAYTRACER {
 
 struct Vec3 {
-  double x,y,z;
-  Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
-  Vec3 operator + (const Vec3& v) const { return Vec3(x+v.x, y+v.y, z+v.z); }
-  Vec3 operator - (const Vec3& v) const { return Vec3(x-v.x, y-v.y, z-v.z); }
-  Vec3 operator * (double d) const { return Vec3(x*d, y*d, z*d); }
-  Vec3 operator / (double d) const { return Vec3(x/d, y/d, z/d); }
-  Vec3 normalize() const {
-    double mg = sqrt(x*x + y*y + z*z);
-    return Vec3(x/mg,y/mg,z/mg);
-  }
+	double x,y,z;
+	Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+	Vec3 operator + (const Vec3& v) const { return Vec3(x+v.x, y+v.y, z+v.z); }
+	Vec3 operator - (const Vec3& v) const { return Vec3(x-v.x, y-v.y, z-v.z); }
+	Vec3 operator * (double d) const { return Vec3(x*d, y*d, z*d); }
+	Vec3 operator / (double d) const { return Vec3(x/d, y/d, z/d); }
+	Vec3 normalize() const {
+		double mg = sqrt(x*x + y*y + z*z);
+		return Vec3(x/mg,y/mg,z/mg);
+	}
 };
 inline double dot(const Vec3& a, const Vec3& b) {
-  return (a.x*b.x + a.y*b.y + a.z*b.z);
+	return (a.x*b.x + a.y*b.y + a.z*b.z);
 }
 
 struct Ray {
-  Vec3 o,d;
-  Ray(const Vec3& o, const Vec3& d) : o(o), d(d) {}
+	Vec3 o,d;
+	Ray(const Vec3& o, const Vec3& d) : o(o), d(d) {}
 };
 
 struct Sphere {
-  Vec3 c;
-  double r;
-  Sphere(const Vec3& c, double r) : c(c), r(r) {}
-  Vec3 getNormal(const Vec3& pi) const { return (pi - c) / r; }
-  bool intersect(const Ray& ray, double &t) const {
-    const Vec3 o = ray.o;
-    const Vec3 d = ray.d;
-    const Vec3 oc = o - c;
-    const double b = 2 * dot(oc, d);
-    const double c = dot(oc, oc) - r*r;
-    double disc = b*b - 4 * c;
-    if (disc < 1e-4) return false;
-    disc = sqrt(disc);
-    const double t0 = -b - disc;
-    const double t1 = -b + disc;
-    t = (t0 < t1) ? t0 : t1;
-    return true;
-  }
+	Vec3 c;
+	double r;
+	Sphere(const Vec3& c, double r) : c(c), r(r) {}
+	Vec3 getNormal(const Vec3& pi) const { return (pi - c) / r; }
+	bool intersect(const Ray& ray, double &t) const {
+		const Vec3 o = ray.o;
+		const Vec3 d = ray.d;
+		const Vec3 oc = o - c;
+		const double b = 2 * dot(oc, d);
+		const double c = dot(oc, oc) - r*r;
+		double disc = b*b - 4 * c;
+		if (disc < 1e-4) return false;
+		disc = sqrt(disc);
+		const double t0 = -b - disc;
+		const double t1 = -b + disc;
+		t = (t0 < t1) ? t0 : t1;
+		return true;
+	}
 };
 
 void clamp255(Vec3& col) {
@@ -65,36 +65,55 @@ void Raytrace() {
 	const Vec3 white(255, 255, 255);
 	const Vec3 black(0, 0, 0);
 	const Vec3 red(255, 0, 0);
+	const Vec3 green(0, 255, 0);
+	const Vec3 blue(0, 0, 255);
 
-	const Sphere sphere(Vec3(W*0.5, H*0.5, 50), 50);
+	const uint8_t spheresSize = 9;
+	const Sphere spheres[spheresSize] = {
+		Sphere(Vec3(W*0.25, H*0.25, 60), 50),
+		Sphere(Vec3(W*0.25, H*0.75, 60), 50),
+		Sphere(Vec3(W*0.25, H*0.50, 60), 50),
+		Sphere(Vec3(W*0.50, H*0.50, 60), 50),
+		Sphere(Vec3(W*0.50, H*0.25, 60), 50),
+		Sphere(Vec3(W*0.50, H*0.75, 60), 50),
+		Sphere(Vec3(W*0.75, H*0.50, 60), 50),
+		Sphere(Vec3(W*0.75, H*0.25, 60), 50),
+		Sphere(Vec3(W*0.75, H*0.75, 60), 50),
 
-	for (int i = 0; i < 15; i++) {
-		const Sphere light(Vec3(i * 50, 0, 50), 1);
+	};
+	for (double i = 0; i < 1; i+= 0.25) {
+		for (double j = 0; j < 1; j+=0.25) {
+			const Sphere light(Vec3(W * i, H * j, 0), 1);
 
-		double t;
-		Vec3 pix_col(black);
+			double t;
+			Vec3 pix_col(black);
 
-		for (int y = 0; y < H; ++y) {
-			for (int x = 0; x < W; ++x) {
-				pix_col = black;
-				const Ray ray(Vec3(x,y,0),Vec3(0,0,1));
+			for (int y = 0; y < H; ++y) {
+				for (int x = 0; x < W; ++x) {
+					pix_col = black;
+					const Ray ray(Vec3(x,y,0),Vec3(0,0,1));
 
-				if (sphere.intersect(ray, t)) {
-					const Vec3 pi = ray.o + ray.d*t;
-					const Vec3 L = light.c - pi;
-					const Vec3 N = sphere.getNormal(pi);
-					const double dt = dot(L.normalize(), N.normalize());
-					pix_col = (red + white*dt) * 0.5;
-					clamp255(pix_col);
+					for (int i = 0; i < spheresSize; i++) {
+						if (spheres[i].intersect(ray, t)) {
+							const Vec3 pi = ray.o + ray.d*t;
+							const Vec3 L = light.c - pi;
+							const Vec3 N = spheres[i].getNormal(pi);
+							const double dt = dot(L.normalize(), N.normalize());
+							if (i < 3) pix_col = (red + white*dt) * 0.5;
+							else if (i < 6) pix_col = (green + white*dt) * 0.5;
+							else pix_col = (blue + white*dt) * 0.5;
+							clamp255(pix_col);
+							break;
+						}
+					}
+
+					uint32_t color = ((uint8_t)pix_col.z | ((uint8_t)pix_col.y << 8) | ((uint8_t)pix_col.x << 16) | (0xff << 24));
+					GOP_print_pixel(x, y, color);
 				}
-
-				uint32_t color = ((uint8_t)pix_col.z | ((uint8_t)pix_col.y << 8) | ((uint8_t)pix_col.x << 16) | (0xff << 24));
-				GOP_print_pixel(x, y, color);
 			}
+
 		}
 	}
-
-	for (int i = 0; i < 30; i++) printf("\n");
 
 	return;
 }
