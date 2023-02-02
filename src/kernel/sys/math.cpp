dbd9cef6f45d8c71f8df81ecbca53f4c3624003e
Filippo Mutta <filippo.mutta@gmail.com>
2023-02-02 18:40:32 +0100

    Added FPU accelerated sqrt.

--- src/kernel/sys/math.cpp
+++ src/kernel/sys/math.cpp
@@ -1,34 +1,24 @@
 #include <math.h>
 
 double sqrt(const double m) {
-	double i=0;
-	double x1,x2;
-	while( (i*i) <= m ) i+=0.1f;
+	if (true) { // x86 FPU
+		double res;
+		asm ("fsqrt" : "=t" (res) : "0" (m));
+		return res;
+	} else {
+		double i=0;
+		double x1,x2;
+		while( (i*i) <= m ) i+=0.1f;
 
-	x1=i;
+		x1=i;
 
-	for(int j=0;j<10;j++) {
-		x2=m;
-		x2/=x1;
-		x2+=x1;
-		x2/=2;
-		x1=x2;
+		for(int j=0;j<10;j++) {
+			x2=m;
+			x2/=x1;
+			x2+=x1;
+			x2/=2;
+			x1=x2;
+		}
+		return x2;
 	}
-	return x2;
-}
-
-int sqrt(int x) {
-	// Base cases
-	if (x == 0 || x == 1)
-	return x;
-
-	// Starting from 1, try all numbers until
-	// i*i is greater than or equal to x.
-	int i = 1, result = 1;
-	while (result <= x) {
-		i++;
-		result = i * i;
-	}
-
-	return i - 1;
 }
