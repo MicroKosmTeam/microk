af97d78d6cc8d11f2ac047d21210aafed75967fe
Filippo Mutta <filippo.mutta@gmail.com>
2023-03-02 13:05:32 +0100

    Memcmp.

---
+++ kernel/mm/memcmp.cpp
@@ -0,0 +1,13 @@
+#include <mm/memory.hpp>
+
+int memcmp(const void* buf1, const void* buf2, size_t count) {
+	if(!count)
+		return(0);
+
+	while(--count && *(char*)buf1 == *(char*)buf2 ) {
+		buf1 = (char*)buf1 + 1;
+		buf2 = (char*)buf2 + 1;
+	}
+
+	return(*((unsigned char*)buf1) - *((unsigned char*)buf2));
+}
