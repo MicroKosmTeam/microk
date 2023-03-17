e985dce6b67e8fabc8f7807ef72feba9bf09e784
Filippo Mutta <filippo.mutta@gmail.com>
2023-03-17 17:20:26 +0100

    Changed comment.

---
+++ module/hello-relocation/main.cpp
@@ -0,0 +1,47 @@
+#include <cdefs.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <sys/driver.hpp>
+
+#include "module.hpp"
+
+const char *MODULE_NAME = "MicroK Test Driver";
+uint64_t *KRNLSYMTABLE;
+Driver *testDriverHeader;
+
+uint64_t Ioctl(uint64_t request, va_list ap) {
+	uint64_t result = 0;
+
+	switch (request) {
+		case 0:
+			PrintK("Hello, world!\r\n");
+			break;
+		default:
+			PrintK("Operation not valid.\r\n");
+			result = 1;
+			break;
+	}
+
+	return result;
+
+}
+
+extern "C" Driver *ModuleInit() {
+	KRNLSYMTABLE = CONFIG_SYMBOL_TABLE_BASE;
+	PrintK = KRNLSYMTABLE[KRNLSYMTABLE_PRINTK];
+	Malloc = KRNLSYMTABLE[KRNLSYMTABLE_MALLOC];
+	Free = KRNLSYMTABLE[KRNLSYMTABLE_FREE];
+	Strcpy = KRNLSYMTABLE[KRNLSYMTABLE_STRCPY];
+
+	PrintK("Hello from %s.\r\n", MODULE_NAME);
+	PrintK("Initializing...\r\n");
+
+	testDriverHeader = new Driver;
+	testDriverHeader->Ioctl = &Ioctl;
+	Strcpy(testDriverHeader->Name, MODULE_NAME);
+
+	PrintK("%s initialization is done. Returning device structure.\r\n", MODULE_NAME);
+
+	return testDriverHeader;
+}
