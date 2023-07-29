01d0324883dd735d09c4d69d2690be53a8a60e36
Filippo Mutta <filippo.mutta@gmail.com>
2023-07-29 15:40:00 +0200

    Changed building script.

--- make.sh
+++ make.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-make -C mkmi static && \
-make -C module/microk-user-module module && \
-make -C module/microk-acpi-module module && \
-make -C module/microk-pci-module module && \
-make -C module/microk-ahci-module module && \
-make -C module/microk-usb-module module && \
-make -C module/microk-doom-demo-module module && \
-make -C microk-kernel kernel && \
-make buildimg && \
+make -C mkmi static
+
+for i in module/*
+do
+	make -C $i module
+done
+
+make -C microk-kernel kernel
+
+make buildimg
+
 make run-x64-efi
 
