9b8ebcc4943c692f1c553ac5c22165ad30cb39a6
Filippo Mutta <filippo.mutta@gmail.com>
2023-07-29 15:38:38 +0200

    Cleaned up modules.

--- make.sh
+++ make.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 make -C mkmi static && \
-make -C module/pci static && \
-make -C module/ext static && \
-make -C module/ahci static && \
-make -C module/acpi static && \
-make -C module/user static && \
+make -C module/microk-user-module module && \
+make -C module/microk-acpi-module module && \
+make -C module/microk-pci-module module && \
+make -C module/microk-ahci-module module && \
+make -C module/microk-usb-module module && \
+make -C module/microk-doom-demo-module module && \
 make -C microk-kernel kernel && \
 make buildimg && \
 make run-x64-efi
